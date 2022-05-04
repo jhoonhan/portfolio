b9c8daf6d068224dd318a682c1a9a8015b73a465
jhoonhan <jhoon.han@outlook.com>
2022-05-03 20:57:56 -0400

    workNav refactored

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -2,10 +2,10 @@ import React, { useRef, useEffect, useState } from "react";
 
 import WorkContent from "./WorkContent";
 import useSubPageStyle from "./useSubPageStyle";
+import WorkNav from "./WorkNav";
 
 const Works = ({ refs, pageControl }) => {
   const [workInfoSubPage, setWorkInfoSubPage] = useState("info");
-
   const { activeSubPageStyle } = useSubPageStyle(workInfoSubPage, pageControl);
 
   const refEl1 = useRef(null);
@@ -13,104 +13,7 @@ const Works = ({ refs, pageControl }) => {
   const refEl3 = useRef(null);
   const refEl4 = useRef(null);
   const refEl5 = useRef(null);
-
-  useEffect(() => {
-    if (pageControl.subWorkPage === "el1") {
-      refEl1.current.scrollIntoView({ behavior: "smooth" });
-    }
-    if (pageControl.subWorkPage === "el2") {
-      refEl2.current.scrollIntoView({ behavior: "smooth" });
-    }
-    if (pageControl.subWorkPage === "el3") {
-      refEl3.current.scrollIntoView({ behavior: "smooth" });
-    }
-    if (pageControl.subWorkPage === "el4") {
-      refEl4.current.scrollIntoView({ behavior: "smooth" });
-    }
-    if (pageControl.subWorkPage === "el5") {
-      refEl5.current.scrollIntoView({ behavior: "smooth" });
-    }
-    setWorkInfoSubPage("info");
-  }, [pageControl.subWorkPage]);
-
-  const renderContentNav = () => {
-    const activeClass = () => {
-      if (pageControl.curPage === "works") {
-        return "active-box";
-      } else {
-        return "";
-      }
-    };
-
-    const active2 = (page) => {
-      let style = {};
-      if (pageControl.curPage === "works") style.transform = "translateX(0vw)";
-
-      if (workInfoSubPage === page) style.opacity = "1";
-      return style;
-    };
-
-    const barWidth = () => {
-      let width;
-      if (workInfoSubPage === "info") width = "5vw";
-      if (workInfoSubPage === "detail") width = "25vw";
-      if (workInfoSubPage === "more") width = "75vw";
-
-      return { width };
-    };
-
-    return (
-      <div
-        className={`${activeClass()} works__content-nav`}
-        style={
-          pageControl.curPage !== "works"
-            ? { transform: "translateX(100vw)" }
-            : {}
-        }
-      >
-        <div className="bar disabled" />
-        <div className="bar active" style={barWidth()} />
-        <div className="labels">
-          <div
-            className={`label info a--opacity ${activeClass()}`}
-            style={active2("info")}
-          >
-            <div className="box" />
-            <span onClick={() => setWorkInfoSubPage("info")}>Info</span>
-          </div>
-          <div
-            className={`label detail a--opacity ${activeClass()}`}
-            style={active2("detail")}
-          >
-            <div className="box" />
-            <span onClick={() => setWorkInfoSubPage("detail")}>Detail</span>
-          </div>
-          <div
-            className={`label more a--opacity ${activeClass()}`}
-            style={active2("more")}
-          >
-            <div className="box" />
-            <span>More Works</span>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const onNextClick = () => {
-    // console.log(workInfoSubPage);
-    if (workInfoSubPage === "info") setWorkInfoSubPage("detail");
-    if (workInfoSubPage === "detail") setWorkInfoSubPage("info");
-  };
-
-  const renderContentArrow = () => {
-    return (
-      <div className="works__content__arrow" onClick={onNextClick}>
-        <span className="arrow"></span>
-        {/* <span className="text">next</span> */}
-      </div>
-    );
-  };
+  const workRefs = { refEl1, refEl2, refEl3, refEl4, refEl5 };
 
   const render = () => {
     return (
@@ -126,8 +29,12 @@ const Works = ({ refs, pageControl }) => {
           <WorkContent refEl={refEl4} />
           <WorkContent refEl={refEl5} />
         </section>
-        {renderContentNav()}
-        {renderContentArrow()}
+        <WorkNav
+          pageControl={pageControl}
+          workRefs={workRefs}
+          workInfoSubPage={workInfoSubPage}
+          setWorkInfoSubPage={setWorkInfoSubPage}
+        />
       </>
     );
   };
