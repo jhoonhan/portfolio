6237ecedb3dd7baf0448cc70a831444ac7996354
jhoonhan <jhoon.han@outlook.com>
2022-05-07 16:22:09 -0400

    refactoring slides

---
+++ src/assests/data/salvationArmy.js
@@ -0,0 +1,24 @@
+import landing from "../image/projects/salvationArmy/landing.jpg";
+
+import overviewImg0 from "../image/projects/salvationArmy/overview0.jpg";
+import overviewImg1 from "../image/projects/salvationArmy/overview1.jpg";
+// import overviewImg2 from "../image/projects/salvationArmy/overview2.jpg";
+import slideImg0 from "../image/projects/salvationArmy/desktop0.jpg";
+import slideImg1 from "../image/projects/salvationArmy/desktop1.jpg";
+import slideImg2 from "../image/projects/salvationArmy/desktop2.jpg";
+
+export const SalvationArmy = {
+  name: ["salvation", " army"],
+  description:
+    "A microsite showcasing stories of customers of a concept pension provider.",
+  role: "senior developer",
+  technology: "HTML5, CSS3, SASS, JQUERY",
+  liveDemoURL: "https://google.com",
+  githubURL: "https://github.com/",
+  images: {
+    landing,
+    overviewImages: [slideImg0, slideImg1, slideImg2],
+    slideImages: [slideImg0, slideImg1, slideImg2],
+  },
+  theme: "light",
+};
