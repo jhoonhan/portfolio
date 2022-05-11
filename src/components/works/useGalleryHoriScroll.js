1a98f3aaeb736533949bdea2538c7cc51527e177
jhoonhan <jhoon.han@outlook.com>
2022-05-11 15:14:11 -0400

    horizontal scroll

--- src/components/works/useGalleryHoriScroll.js
+++ src/components/works/useGalleryHoriScroll.js
@@ -6,51 +6,50 @@ const useGalleryHoriScroll = (pageControl) => {
   useEffect(() => {
     if (pageControl.workSubPage !== "detail") return;
     const el = elRef.current;
+    if (!el) return;
+    el.scrollTo({ left: 0 });
 
-    if (el) {
-      el.scrollTo({ left: 0 });
+    const onWheel = (e) => {
+      // console.log(e.deltaY);
+      e.preventDefault();
 
-      const onWheel = (e) => {
-        // console.log(e.deltaY);
-        e.preventDefault();
+      if (e.deltaY === 0) return;
+      el.scroll({
+        left: el.scrollLeft + e.deltaY * 7,
+        behavior: "smooth",
+      });
+    };
 
-        if (e.deltaY === 0) return;
-        el.scrollTo({
-          left: el.scrollLeft + e.deltaY * 7,
-          behavior: "smooth",
-        });
-      };
+    const fn = (e) => {
+      console.log(e);
+      const child = el.children[0].getBoundingClientRect();
+      if (
+        el.scrollLeft > 0 &&
+        Math.floor(window.innerWidth) < Math.round(child.right) - 0
+      ) {
+        e.stopPropagation();
+      }
 
-      const fn = (e) => {
-        const child = el.children[0].getBoundingClientRect();
-        if (
-          el.scrollLeft > 0 &&
-          Math.floor(window.innerWidth) < Math.round(child.right) - 0
-        ) {
-          e.stopPropagation();
-        }
+      const s = el.scrollLeft + e.deltaY * 7;
+      const w = child.width;
+      const vw = window.innerWidth;
+      const x = s / (w - vw + 700);
 
-        const s = el.scrollLeft + e.deltaY * 7;
-        const w = child.width;
-        const vw = window.innerWidth;
-        const x = s / (w - vw + 700);
+      const amount = 0.6 * x * 100;
+      if (s <= 0) {
+        pageControl.setWorkNavWidth(`${25}%`);
+      }
+      if (s > 0) {
+        pageControl.setWorkNavWidth(`${25 + amount}%`);
+      }
 
-        const amount = 0.6 * x * 100;
-        if (s <= 0) {
-          pageControl.setWorkNavWidth(`${25}%`);
-        }
-        if (s > 0) {
-          pageControl.setWorkNavWidth(`${25 + amount}%`);
-        }
+      onWheel(e);
+    };
 
-        onWheel(e);
-      };
-
-      el.addEventListener("mousewheel", fn);
-      return () => {
-        el.removeEventListener("mousewheel", fn);
-      };
-    }
+    el.addEventListener("mousewheel", fn);
+    return () => {
+      el.removeEventListener("mousewheel", fn);
+    };
   }, [pageControl.workSubPage]);
   return elRef;
 };
