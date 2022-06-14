1fa11a9e25599be5b769fb81d8cfb4c559aef79b
jhoonhan <jhoon.han@outlook.com>
2022-06-14 11:57:17 -0400

    somewhat working

--- src/components/helpers/DesktopSVG.jsx
+++ src/components/helpers/DesktopSVG.jsx
@@ -30,7 +30,7 @@ const DesktopSVG = ({ data, type, customClass }) => {
   };
   return (
     <div className={`slide-img--desktop ${customClass}`}>
-      <svg
+      {/* <svg
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         // xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -83,8 +83,45 @@ const DesktopSVG = ({ data, type, customClass }) => {
 		c13.44,0,24.33-10.89,24.33-24.33V24.33C1400,10.89,1389.11,0,1375.67,0L1375.67,0z"
           />
         </g>
-      </svg>
+      </svg> */}
       <div className="masking">{renderVisual()}</div>
+
+      <svg
+        id="computer"
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 350 350"
+      >
+        <defs>
+          <linearGradient
+            id="linear-gradient"
+            x1="175"
+            y1="312.22"
+            x2="175"
+            y2="331.11"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.3" stopColor="#ccc" />
+            <stop offset="0.42" stopColor="#c8c8c8" />
+            <stop offset="0.55" stopColor="#bcbcbc" />
+            <stop offset="0.67" stopColor="#a8a8a8" />
+            <stop offset="0.8" stopColor="#8b8b8b" />
+            <stop offset="0.92" stopColor="#676767" />
+            <stop offset="1" stopColor="#4d4d4d" />
+          </linearGradient>
+        </defs>
+        <rect width="350" height="340.55" style={{ fill: "#ccc" }} />
+        <path
+          d="M0,331.11H350a0,0,0,0,1,0,0V344a6,6,0,0,1-6,6H6a6,6,0,0,1-6-6V331.11A0,0,0,0,1,0,331.11Z"
+          style={{ fill: "#b3b3b3" }}
+        />
+        <rect
+          y="312.22"
+          width="350"
+          height="18.89"
+          style={{ fill: "url(#linear-gradient)" }}
+        />
+      </svg>
     </div>
   );
 };
